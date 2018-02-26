9c4950b930867538e00bbaa55f18987e2531cd1d
suufi <qxest@reqxest.com>
2018-02-25 19:35:34 -0500

    Add the Roblox module

---
+++ roblox/WebhookModule.lua
@@ -0,0 +1,20 @@
+local WebhookModule = {}
+
+local HTTPService = game:GetService('HttpService');
+local API_URL = "apiURL + port";
+
+function WebhookModule:Send( Content )
+	Content = HTTPService:JSONEncode( Content );	
+	
+	local Post = pcall(function()
+		HTTPService:PostAsync(API_URL .. '/discord/webhook', Content);
+	end)
+		
+	if Post then
+		return "✅ Webhook has posted successfully";
+	else
+		return ("❌ Webhook has failed to post " .. Content);
+	end
+end
+
+return WebhookModule
